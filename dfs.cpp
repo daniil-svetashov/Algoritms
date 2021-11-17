1e8f07fe5aa9a3959d698ee11312f19cf21c8d8a
daniil-svetashov <oodessa13@gmail.com>
2021-11-17 04:32:38 +0200

    v2 - not finished, but works ok

--- dfs.cpp
+++ dfs.cpp
@@ -5,15 +5,17 @@ using namespace std;
 const int V = 8;
 const int INF = 9999999;
 
+
+//matrix from page 12 TA_6_1_21.pdf
 int G[V][V] = {
-    {INF, INF, 2, 1, INF, 8, INF, INF},
-    {INF, INF, 1, 6, INF, 3, 4, INF},
-    {2, 1, INF, INF ,INF, INF, INF, 4},
-    {1, 6, INF, INF, 4, INF, INF, INF},
-    {INF, INF, INF, 4, INF, INF, 7, INF},
-    {8, 3, INF, INF, INF, INF, INF, 3},
-    {INF, 4, INF, INF, 7, INF, INF, 5},
-    {INF, INF, 4, INF, INF, 3, 5, INF}
+    {INF, 1, 1, 1, INF, INF, INF, INF},
+    {1, INF, INF, INF, 1, INF, INF, INF},
+    {1, INF, INF, INF ,INF, INF, INF, INF},
+    {1, INF, INF, INF, INF, 1, INF, INF},
+    {INF, 1, INF, INF, INF, 1, INF, INF},
+    {INF, INF, INF, 1, 1, INF, 1, 1},
+    {INF, INF, INF, INF, INF, 1, INF, INF},
+    {INF, INF, INF, INF, INF, 1, INF, INF}
 };
 
 int main()
@@ -26,7 +28,9 @@ int main()
 	
 	int start=0;//It will be our start vertex
 	//for this example our start vertex will be 0
-	visited[start]=true;
+	
+	//!!!!!!/////visited[start]=true; WE DONT NEED IT !
+	
 	//we go threw the colomns and find valuebalse which are not
 	//equel infinity
 	//if vertex has valueble but it is connected to the vertex
@@ -54,18 +58,19 @@ int main()
 
 	bool all_visited;
 
+	cout<<"WE START FROM: "<<start<<" ,then"<<endl;
 	do
 	{
-		counter=0;
+		//counter=0;
 		all_visited=true;
 
 
-	for(int i=0;i<V;i++)
+	for(int i=0;i<V;i++,counter=0)
 	{
 		if(G[current][i]!=INF && visited[i]==false)
 	  {
 		  current=i;
-		  cout<<"We are in vertex: "<<i<<endl;
+		  cout<<"we are in vertex: "<<i<<endl;
 		  visited[i]=true;
 		  path_index++;
 		  path[path_index]=i;
@@ -77,18 +82,18 @@ int main()
 		  //the start and dont have adjent vertexes,we have
 		  //finished th ealgorithms 
 	  {
-		  cout<<"We came to the previous vertex"<<endl;
 		  path_index--;
 		  current=path[path_index];
+	           cout<<"We came to the previous vertex:"<<path[path_index]<<endl;
+
 	  }
 
 
 	  for(int i=0;i<V;i++)
 	  {
-		  if(visited[i]=false)
+		  if(visited[i]==false)
 			  all_visited=false;
 	  }
-
 	}while(!all_visited);
 
 
